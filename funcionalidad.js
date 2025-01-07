9a5f1b581706a3378aa3d65acdef9228ea39458a
albertico9706 <abcelkuqui@gmail.com>
2025-01-07 18:22:42 -0500

    resolved challenge

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,3 +1,29 @@
-const funcionPrueba = () => {
-  console.log('Prueba técnica');
-};
+import {get_spacexdata} from "./lib/api.js"
+import * as schemas from "./lib/schemas.js"
+import { create_elements } from "./lib/util.js"
+
+
+await populate_board("lanpad")
+
+async function populate_board(classname){
+  const LOCATIONS={
+    INDEX:"/gitproyects/soluciones_lab/prueba-tecnica-junior/"
+  }
+  const pathname=window.location.pathname===LOCATIONS.INDEX? "lanpads":window.location.pathname.split("/").at(4)
+  console.log(pathname)
+  const data=await get_spacexdata(pathname)
+  const schema=schemas[pathname]
+  data.map((d)=>{
+      const elements=create_elements(d,schema)
+      const pieze=document.createElement("div");
+      pieze.classList.add(classname)
+
+      for (let el of elements){
+        pieze.append(el)}
+
+      document.querySelector(".board").appendChild(pieze);
+  })
+
+
+}
+
